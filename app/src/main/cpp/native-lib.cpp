f9d810288350950e3c90df8506d2fc739b778b89
zhangtao <fangziyi199110@163.com>
2024-06-10 23:46:16 +0800

    集成了x264的库

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,11 +1,16 @@
 #include <jni.h>
 #include <string>
 #include <rtmp.h>
+#include <x264.h>
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_rtmp_androidapp_MainActivity_stringFromJNI(
-        JNIEnv* env,
+        JNIEnv *env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+    jint version = RTMP_LibVersion();
+    char buffer[40];
+    sprintf(buffer, "rtmp的版本是：%d", version);
+    x264_picture_t *p = new x264_picture_t;
+    return env->NewStringUTF(buffer);
 }
